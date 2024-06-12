e812be70b75f7b3dd4abb5d92b422d4031448854
lexus <mrkethss@gmail.com>
2024-06-12 15:11:33 +0300

    Добавьте файлы проекта.

---
+++ VISELICCAIOOU/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace VISELICCAIOOU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 420);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(418, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "НАЧАТЬ ИГРУ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(418, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 50);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(418, 317);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(301, 50);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Cyan;
+            this.button2.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(418, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(301, 59);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ДАТЬ ОТВЕТ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(418, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 156);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "VISELICA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VISELICCAIOOU/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VISELICCAIOOU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        public string text;
+        public char[] word;
+        public int hp;
+        public int hptrue;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+            hp = 0;
+            label1.Text = null;
+            textBox1.Text = null;
+            word = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                label1.Text += '.';
+                word[i] = '.';
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool words = false;
+            int j = 0;
+            if (textBox2.Text.Length == 1)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (textBox2.Text[0] == text[i])
+                    {
+                        words = true;
+                        hptrue++;
+                        word[i] = text[i];
+                        label1.Text = null;
+                    }
+                }
+                if (words == true)
+                {
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        label1.Text += word[i];
+                    }
+                    if (hptrue == text.Length)
+                    {
+                        MessageBox.Show("Ты победил!");
+                    }
+                }
+                else
+                {
+                    hp++;
+                    string hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    if (hp == 5)
+                    {
+                        MessageBox.Show("Ты проиграл!");
+                        label1.Text = null;
+                        label1.Text = text;
+                    }
+                }
+
+            }
+            else if (textBox2.Text.Length > 1)
+            {
+                if (textBox2.Text == text)
+                {
+                    label1.Text = null;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        label1.Text += text[i];
+                    }
+                    MessageBox.Show("Ты победил!!");
+                }
+                else
+                {
+                    hp++;
+                    string hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    if (hp == 5)
+                    {
+                        MessageBox.Show("Ты проиграл!");
+                        label1.Text = null;
+                        label1.Text = text;
+                    }
+                }
+                textBox2.Text = null;
+            }
+        }
+    }
+}
